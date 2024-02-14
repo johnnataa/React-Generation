0da04c6739e7bdffdbe01ed57e5d8bc71cb5fa57
johnnataa <johnnata_@outlook.com>
2024-02-14 13:27:05 -0300

    first commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+    <h1 className="titulo">Game of Thrones</h1>
+    <img className="img" src="https://upload.wikimedia.org/wikipedia/pt/a/a0/GameofThrones.png" alt="Imagem de abertura da série Game of Thrones" />
+    </>
+  )
+}
+
+export default Home;
